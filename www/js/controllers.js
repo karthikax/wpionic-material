f23b7b53e5a4baee9ca0c39ea03fda528d019af1
Karthik B <karthik.ax.ax@gmail.com>
2015-08-31 15:51:34 +0530

    Menu Classes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,15 +2,12 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-// With the new view caching in Ionic, Controllers are only called
-// when they are recreated or on app start, instead of every page change.
-// To listen for when this page is active (for example, to refresh data),
-// listen for the $ionicView.enter event:
-//$scope.$on('$ionicView.enter', function(e) {
-//});
-
-// Form data for the login modal
+$scope.isExpanded = false;
 $scope.loginData = {};
+
+$scope.setExpanded = function(bool) {
+    $scope.isExpanded = bool;
+};
 
 // Create the login modal that we will use later
 $ionicModal.fromTemplateUrl('templates/login.html', {
